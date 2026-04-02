93c31d6a432c8113484fae37d6a631cd4e14f727
Nishikanth <nishikanth.uysc.001@gmail.com>
2026-04-02 15:35:10 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
+import { supabase } from '@/lib/supabase'
 
+
+// @ts-ignore
+window.supabase = supabase
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
